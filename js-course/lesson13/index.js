258b906662c38ee01870cac3a5f720ed21103c41
ale maj <45163764+Sudo-Ale@users.noreply.github.com>
2024-02-11 00:17:17 +0100

    more practice on js

---
+++ js-course/lesson13/index.js
@@ -0,0 +1,8 @@
+// method chaining = calling one method after another
+//                   in one continuous line of code
+
+let userName = "ale";
+
+let letter = userName.charAt(0).toUpperCase();
+
+console.log(letter);
---
+++ js-course/lesson16/index.js
@@ -0,0 +1,25 @@
+// switches = statement that examines a value
+//            for a match against many case clauses.
+//            More efficient that many "else if" statements
+
+let grade = "A";
+
+switch(grade){
+    case "A":
+        console.log("You did great!");
+        break;
+    case "B":
+        console.log("You did good!");
+        break;
+    case "C":
+        console.log("You did okay!");
+        break;
+    case "D":
+        console.log("You passed ... barely");
+        break;
+    case "F":
+        console.log("You FAILED!");
+        break;
+    default:
+        console.log(grade, "is not a letter grade");
+}
