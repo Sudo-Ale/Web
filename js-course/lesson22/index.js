60999a39ba765f007b7d287378d1bf1de8c16e64
ale maj <45163764+Sudo-Ale@users.noreply.github.com>
2024-02-12 00:50:28 +0100

    more js course lessons

---
+++ js-course/lesson22/index.js
@@ -0,0 +1,12 @@
+// break = breaks out of a loop entirely
+// continue = skip an iteration of a loop
+
+for(let i = 0; i <= 20; i+=1){
+    // if(i == 13){
+    //     break;
+    // }
+    if(i == 13){
+        continue;
+    }
+    console.log(i);
+}
---
+++ js-course/lesson29/index.js
@@ -0,0 +1,7 @@
+// toLocaleString() = returns a string with a language
+//                    sensitive representation of this number
+
+// number.ToLocaleString(locale, {options});
+
+// 'locale' = specify that language (undefined = default set in)
+// 'options' = object with formatting options
